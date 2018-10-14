a87586b569d2d185d81de6a8918ef53b9060a24d
Gunther Cox <gunthercx@gmail.com>
2018-10-14 12:54:14 -0400

    Create simple stemming algorithm

---
+++ chatterbot/stemming.py
@@ -0,0 +1,37 @@
+import string
+import nltk
+
+
+class RidiculouslySimpleStemmer(object):
+
+    def __init__(self, language='english'):
+        self.punctuation_table = str.maketrans(dict.fromkeys(string.punctuation))
+
+        # Get list of stopwords from the NLTK corpus
+        self.stopwords = nltk.corpus.stopwords.words(language)
+        self.stopwords.append('')
+
+    def stem(self, text):
+
+        # Remove punctuation
+        text = text.translate(self.punctuation_table)
+
+        # Make the text lowercase
+        text = text.lower()
+
+        words = []
+
+        # Generate the stemmed text
+        for word in text.split(' '):
+
+            # Remove stopwords
+            if word not in self.stopwords:
+
+                # Chop off the ends of the word
+                start = len(word) // 4
+                stop = start * -1
+                word = word[start:stop]
+
+                words.append(word)
+
+        return ' '.join(words)[:-1]
